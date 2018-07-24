7564a2401801d224e638c14c810fe1465940b221
章建伟 <zhangjianwei@zhangjieideiMac.lan>
2018-07-24 10:44:22 +0800

    add new

--- Ex1.3/src/FixedCapacityStackOfStrings.java
+++ Ex1.3/src/FixedCapacityStackOfStrings.java
@@ -1,28 +1,62 @@
-public class FixedCapacityStackOfStrings {
-    private String[] a;
-    private int n; //size
-    public FixedCapacityStackOfStrings(int cap){
-        a = new String[cap];
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class FixedCapacityStackOfStrings implements Iterable<String> {
+    private String[] a;  // holds the items
+    private int N;       // number of items in stack
+
+    // create an empty stack with given capacity
+    public FixedCapacityStackOfStrings(int capacity) {
+        a = new String[capacity];
+        N = 0;
     }
 
-    public boolean isEmpty(){
-        return n == 0;
+    public boolean isEmpty()            {  return N == 0;                    }
+    public boolean isFull()             {  return N == a.length;             }
+    public void push(String item)       {  a[N++] = item;                    }
+    public String pop()                 {  return a[--N];                    }
+    public String peek()                {  return a[N-1];                    }
+    public Iterator<String> iterator()  { return new ReverseArrayIterator(); }
+
+
+    public class ReverseArrayIterator implements Iterator<String> {
+        private int i = N-1;
+
+        public boolean hasNext() {
+            return i >= 0;
+        }
+
+        public String next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return a[i--];
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
 
-    public int size(){
-        return n;
-    }
 
-    public void push(String item){
-        a[n++] = item;
-    }
+    public static void main(String[] args) {
+        int max = Integer.parseInt(args[0]);
+        FixedCapacityStackOfStrings stack = new FixedCapacityStackOfStrings(max);
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) stack.push(item);
+            else if (stack.isEmpty())  StdOut.println("BAD INPUT");
+            else                       StdOut.print(stack.pop() + " ");
+        }
+        StdOut.println();
 
-    public String pop(){
-        return a[--n];
-    }
-
-    public boolean isFull()
-    {
-        return n == a.length;
+        // print what's left on the stack
+        StdOut.print("Left on stack: ");
+        for (String s : stack) {
+            StdOut.print(s + " ");
+        }
+        StdOut.println();
     }
 }
---
+++ Ex1.3/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.PrintStream;
+import java.io.FileOutputStream;
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        File outFile = new File("stdout.txt");
+        PrintStream ps = new PrintStream(new FileOutputStream(outFile));
+
+        System.out.println(outFile.getAbsolutePath());
+
+        System.setOut(ps);
+
+        System.out.println("Hello world!");
+        System.out.println("Java I/O  is cool!");
+    }
+}
