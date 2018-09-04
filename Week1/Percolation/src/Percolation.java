7491be1e5c2e9e9f1cd14d52646900ff3b7a3ec0
Jianwei <jwzhang86@gmail.com>
2018-09-04 13:06:49 -0400

    new

--- Week1/Percolation/src/Percolation.java
+++ Week1/Percolation/src/Percolation.java
@@ -2,7 +2,18 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 //定义 Percolation 的数据结构
 public class Percolation {
     private WeightedQuickUnionUF grid;
+    private WeightedQuickUnionUF gridBack;
+    private int N;
+    private boolean[] NN; //数值类型为布尔型，因为水槽要么有水要么没有水
+
     public Percolation(int n){
+       this.N = N;
+       NN = new boolean[N*N];
+       grid = new WeightedQuickUnionUF(N*N + 4);
+       gridBack = new WeightedQuickUnionUF(N*N + 1);
+       for(int i =0; i<N;i++)
+           for(int j =0; j<N; j++)
+               NN[i*N+j] = false;
     }                // create n-by-n grid, with all sites blocked
 
     public void open(int row, int col){
