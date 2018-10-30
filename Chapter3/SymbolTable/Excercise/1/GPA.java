5c846c2d0b2c82c4192d41a3b3565ce9ba47900b
章建伟 <zhangjianwei@akiradates-MacBook-puro.local>
2018-10-29 20:27:02 -0400

    finishing homework

---
+++ Chapter3/SymbolTable/Excercise/1/GPA.java
@@ -0,0 +1,29 @@
+import edu.princeton.cs.algs4.ST;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class GPA{
+	public static void main(String[] args){
+		ST<String,Double> grades = new ST<String, Double>();
+		grades.put("A",  4.00);
+        grades.put("B",  3.00);
+        grades.put("C",  2.00);
+        grades.put("D",  1.00);
+        grades.put("F",  0.00);
+        grades.put("A+", 4.33);
+        grades.put("B+", 3.33);
+        grades.put("C+", 2.33);
+        grades.put("A-", 3.67);
+        grades.put("B-", 2.67);
+
+        int n = 0;
+        double total = 0.0;
+        for (n = 0; !StdIn.isEmpty(); n++) {
+            String grade = StdIn.readString();
+            double value = grades.get(grade);
+            total += value;
+        }
+        double gpa = total / n;
+        StdOut.println("GPA = " + gpa);
+    }
+}
